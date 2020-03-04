ac8ec5ae4f92ebd6311caa478cb93fd5c25272ea
alf15107 <47034593+alf15107@users.noreply.github.com>
2020-03-03 21:05:00 -0500

    Update TravProf.java

--- src/TravProf.java
+++ src/TravProf.java
@@ -1,26 +1,5 @@
 import java.util.Scanner;
 public class TravProf {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Welcome! Enter traveler ID: ");
-        String travAgentID = in.nextLine();
-        // if travAgentID is already in the db, give options to get or update
-        // if not, ask questions:
-        System.out.println("Enter first name: ");
-        String firstName = in.nextLine();
-        System.out.println("Enter last name: ");
-        String lastName = in.nextLine();
-        System.out.println("Enter address: ");
-        String address = in.nextLine();
-        System.out.println("Enter phone number: ");
-        String phone = in.nextLine();
-        System.out.println("Enter cost of trip: ");
-        Float tripCost = in.nextLine();
-        System.out.println("Enter type of travel: ");
-        String travelType = in.nextLine();
-        System.out.println("Enter type of payment: ");
-        String paymentType = in.nextLine();
-        // this is where medcondinfo will go
-        //System.out.println("Enter medical condition information: ");
+    
 }
 }
