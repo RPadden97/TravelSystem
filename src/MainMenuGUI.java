83f73bf0b90770880ca21bf92cbaf9f7e5d8c308
alf15107 <47034593+alf15107@users.noreply.github.com>
2020-04-27 19:28:12 -0400

    Update MainMenuGUI.java

--- src/MainMenuGUI.java
+++ src/MainMenuGUI.java
@@ -2,9 +2,9 @@ import javax.swing.*;
 
 public class MainMenuGUI {
     private JFrame mainFrame;
-    private JLabel lb1;
-    private JButton btn;
-    private JRadioButton jradio_btn1, jradio_btn2, jradio_btn3, jradio_btn4, jradio_btn5;
+    private JLabel lb_mm;
+    private JButton btn_mm;
+    private JRadioButton jradio_btn1_mm, jradio_btn2_mm, jradio_btn3_mm, jradio_btn4_mm, jradio_btn5_mm;
     //create a constructor
     public MainMenuGUI(){
         prepareGUI();
@@ -13,32 +13,32 @@ public class MainMenuGUI {
         mainFrame = new JFrame("Main Menu");
         mainFrame.setSize(400,400);
         mainFrame.setLayout(null);
-        lb1 = new JLabel("Integrated Travel System");
-        lb1.setBounds(120, 50, 150, 20);
-        jradio_btn1 = new JRadioButton();
-        jradio_btn1.setText("Create Profile");
-        jradio_btn1.setBounds(100, 90, 120, 20);
-        jradio_btn2 = new JRadioButton();
-        jradio_btn2.setText("Delete Profile");
-        jradio_btn2.setBounds(100, 120, 120, 20);
-        jradio_btn3 = new JRadioButton();
-        jradio_btn3.setText("Update Profile");
-        jradio_btn3.setBounds(100, 150, 120, 20);
-        jradio_btn4 = new JRadioButton();
-        jradio_btn4.setText("Find/Display Profile");
-        jradio_btn4.setBounds(100, 180, 150, 20);
-        jradio_btn5 = new JRadioButton();
-        jradio_btn5.setText("Display All Profiles");
-        jradio_btn5.setBounds(100, 210, 150, 20);
-        btn = new JButton("Select");
-        btn.setBounds(120, 250, 100, 20);
-        mainFrame.add(btn);
-        mainFrame.add(lb1);
-        mainFrame.add(jradio_btn1);
-        mainFrame.add(jradio_btn2);
-        mainFrame.add(jradio_btn3);
-        mainFrame.add(jradio_btn4);
-        mainFrame.add(jradio_btn5);
+        lb_mm = new JLabel("Integrated Travel System");
+        lb_mm.setBounds(120, 50, 150, 20);
+        jradio_btn1_mm = new JRadioButton();
+        jradio_btn1_mm.setText("Create Profile");
+        jradio_btn1_mm.setBounds(100, 90, 120, 20);
+        jradio_btn2_mm = new JRadioButton();
+        jradio_btn2_mm.setText("Delete Profile");
+        jradio_btn2_mm.setBounds(100, 120, 120, 20);
+        jradio_btn3_mm = new JRadioButton();
+        jradio_btn3_mm.setText("Update Profile");
+        jradio_btn3_mm.setBounds(100, 150, 120, 20);
+        jradio_btn4_mm = new JRadioButton();
+        jradio_btn4_mm.setText("Find/Display Profile");
+        jradio_btn4_mm.setBounds(100, 180, 150, 20);
+        jradio_btn5_mm = new JRadioButton();
+        jradio_btn5_mm.setText("Display All Profiles");
+        jradio_btn5_mm.setBounds(100, 210, 150, 20);
+        btn_mm = new JButton("Select");
+        btn_mm.setBounds(120, 250, 100, 20);
+        mainFrame.add(btn_mm);
+        mainFrame.add(lb_mm);
+        mainFrame.add(jradio_btn1_mm);
+        mainFrame.add(jradio_btn2_mm);
+        mainFrame.add(jradio_btn3_mm);
+        mainFrame.add(jradio_btn4_mm);
+        mainFrame.add(jradio_btn5_mm);
         mainFrame.setVisible(true);
     }
     public static void main(String[] args){
